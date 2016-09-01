9fb2b244b04e62880e610b5c8b1b6d0b29ebf819
YanceyYu <yuyang_053@163.com>
2016-09-01 14:45:40 +0800

    add notice reply

--- src/main/java/com/favorites/web/NoticeController.java
+++ src/main/java/com/favorites/web/NoticeController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.favorites.domain.Comment;
+import com.favorites.domain.CommentRepository;
+import com.favorites.domain.Notice;
 import com.favorites.domain.NoticeRepository;
 import com.favorites.domain.result.ExceptionMsg;
 import com.favorites.domain.result.Response;
+import com.favorites.utils.DateUtils;
 
 /**
 *@ClassName: NoticeController
@@ -24,6 +28,13 @@ public class NoticeController extends BaseController{
 	@Autowired
 	private NoticeRepository noticeRepository;
 	
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	/**
+	 * 更新消息为已读
+	 * @return
+	 */
 	@RequestMapping(value = "/updateAtMeNoticeReaded", method = RequestMethod.POST)
 	public Response updateAtMeNoticeReaded() {
 		logger.info("updateAtMeNoticeReaded begin");
@@ -37,5 +48,32 @@ public class NoticeController extends BaseController{
 		return result();
 	}
 	
+	/**
+	 * 回复
+	 * @param comment
+	 * @return
+	 */
+	@RequestMapping(value = "/reply", method = RequestMethod.POST)
+	public Response reply(Comment comment) {
+		logger.info("reply begin");
+		try {
+			comment.setUserId(getUserId());
+			comment.setCreateTime(DateUtils.getCurrentTime());
+			Comment saveCommon = commentRepository.save(comment);
+			Notice notice = new Notice();
+			notice.setCollectId(comment.getCollectId().toString());
+			notice.setUserId(comment.getReplyUserId());
+			notice.setType("comment");
+			notice.setReaded("unread");
+			notice.setOperId(saveCommon.getId().toString());
+			notice.setCreateTime(DateUtils.getCurrentTime());
+			noticeRepository.save(notice);
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.error("updateUserName failed, ", e);
+			return result(ExceptionMsg.FAILED);
+		}
+		return result();
+	}
 
 }
