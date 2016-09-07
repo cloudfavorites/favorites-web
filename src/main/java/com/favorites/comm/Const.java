bf3284b006e6de4e0549fcbd6a84e911de90b0a2
Smilence1024 <1204329367@qq.com>
2016-09-07 14:38:39 +0800

    fix findbugs

--- src/main/java/com/favorites/comm/Const.java
+++ src/main/java/com/favorites/comm/Const.java
@@ -23,7 +23,7 @@ public class Const {
 
 	
 	  @Autowired(required = true)
-	  public void setBasePath(@Value("${favorites.base.path}")String basePath) {
+	  public static void setBasePath(@Value("${favorites.base.path}")String basePath) {
 		  Const.BASE_PATH = basePath;
 	  }
 	
