2f896423c57c9bf38cfcbefea44a8b2f9b927d0a
YanceyYu <yuyang_053@163.com>
2017-02-10 17:56:40 +0800

    add /user

--- src/main/java/com/favorites/comm/filter/SecurityFilter.java
+++ src/main/java/com/favorites/comm/filter/SecurityFilter.java
@@ -158,7 +158,7 @@ public class SecurityFilter implements Filter {
                 || url.contains("/register")||url.contains("/user/regist")||url.contains("/index")
                 || url.contains("/forgotPassword")||url.contains("/user/sendForgotPasswordEmail")
                 || url.contains("/newPassword")||url.contains("/user/setNewPassword")
-				|| url.contains("/collect")|| url.contains("/favorites")||url.contains("/comment")
+				|| url.contains("/collect")||url.contains("/user")||url.contains("/favorites")||url.contains("/comment")
 				|| url.startsWith("/lookAround/standard/")
 				|| url.startsWith("/lookAround/simple/")
 				|| url.startsWith("/user/")
